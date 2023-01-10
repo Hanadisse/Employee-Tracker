bd02800df0846e62352c9bd88c72be906a58f1a3
hanad isse <hanadisse1@gmail.com>
2023-01-10 15:32:10 -0600

    set up function for updateing already existing employees role

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function menu() {
       } else if (res.choice == "Add a Employee") {
         addEmployee();
       } else if (res.choice == "Update a Employee") {
-        // updateEmployee();
+        updateEmployee();
       } else if (res.choice == "Quit") {
         console.log("Goodbye!");
       }
@@ -168,4 +168,68 @@ function addEmployee() {
       console.log(`Added ${res.firstName} ${res.lastName} to the database`);
       menu();
     });
+}
+
+// Update Employee Role
+function updateEmployee() {
+  db.query(`SELECT * FROM employee`, (err, data) => {
+    if (err) throw err;
+
+    const employees = data.map(({ id, first_name, last_name }) => ({
+      name: first_name + " " + last_name,
+      value: id,
+    }));
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "name",
+          message: "Which employee would you like to update?",
+          choices: employees,
+        },
+      ])
+      .then((empChoice) => {
+        const employee = empChoice.name;
+        const params = [];
+        params.push(employee);
+
+        db.query(`SELECT * FROM roles`, (err, data) => {
+          if (err) throw err;
+
+          const roles = data.map(({ id, title }) => ({
+            name: title,
+            value: id,
+          }));
+
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "role",
+                message: "What is the employee's new role?",
+                choices: roles,
+              },
+            ])
+            .then((roleChoice) => {
+              const role = roleChoice.role;
+              params.push(role);
+
+              let employee = params[0];
+              params[0] = role;
+              params[1] = employee;
+
+              db.query(
+                `UPDATE employee SET role_id = ? WHERE id = ?`,
+                params,
+                (err) => {
+                  if (err) throw err;
+                  console.log("Employee has been updated!");
+                  menu();
+                }
+              );
+            });
+        });
+      });
+  });
 }
