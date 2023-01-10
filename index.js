99817329cb5177986ae2ecbc8032c8f2a45af436
hanad isse <hanadisse1@gmail.com>
2023-01-10 15:19:52 -0600

    set up initial prompt to ask the user what they want to do

--- index.js
+++ index.js
@@ -15,3 +15,43 @@ db.connect(function (err) {
   console.log("MySQL Connected");
   menu();
 });
+
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          "View all Departments",
+          "View all Roles",
+          "View all Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add a Employee",
+          "Update a Employee",
+          "Quit",
+        ],
+      },
+    ])
+    .then((res) => {
+      if (res.choice == "View all Departments") {
+        // viewDepartments();
+      } else if (res.choice == "View all Roles") {
+        // viewRoles();
+      } else if (res.choice == "View all Employees") {
+        // viewEmployees();
+      } else if (res.choice == "Add a Department") {
+        // addDepartment();
+      } else if (res.choice == "Add a Role") {
+        // addRole();
+      } else if (res.choice == "Add a Employee") {
+        // addEmployee();
+      } else if (res.choice == "Update a Employee") {
+        // updateEmployee();
+      } else if (res.choice == "Quit") {
+        console.log("Goodbye!");
+      }
+    });
+}
